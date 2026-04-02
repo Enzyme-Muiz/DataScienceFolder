a2a12474839fac18e83e6e1d755428f20b51bc7a
Enzyme-Muiz <muizdeenraji@gmail.com>
2026-04-02 08:41:20 +0100

    examples of connection

--- dspackagefolder/template/main.py
+++ dspackagefolder/template/main.py
@@ -41,3 +41,18 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 from src.utils.env_loader import load_env_from_root
 env_path = load_env_from_root()
 print(f"Loaded env variables from: {env_path}")
+
+
+
+### Example usage of SQLCRUD
+import sys
+from pathlib import Path    
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from src.sqlcrud import SQLServerCRUD
+connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=your_server;DATABASE=your_db;UID=your_user;PWD=your_password"
+sql_crud = SQLServerCRUD(connection_string)
+sql_crud.connect()
+sql_crud.build_query("sql/query.sql", params={"param1": "value1"})
+df = sql_crud.read_data()
+print(df)
+sql_crud.close_connection()
